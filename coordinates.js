5ada539b8e3b80e7c97e9a3c0784c23a0e816b58
trovati <watsontrovati@gmail.com>
2021-07-19 13:48:33 -0300

    First Commit

---
+++ coordinates.js
@@ -0,0 +1,45 @@
+const request = require('request');
+const {endereco} = require('./endereco.json')
+
+const API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+const API_KEY = 'AIzaSyCMHugBqB_VEmy7aPMBLjjG-rrRBGLtmtM';
+
+const parseJson = (string) => {
+
+    try{
+        return JSON.parse(string)
+    }catch(error) {
+        console.error;
+    }
+}
+
+const doRequest = (url) => {
+    const promisse = (resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (error) reject(error);
+            const data = parseJson(body);
+            console.log(data)
+            resolve(data);
+        });
+    };
+    return new Promise(promisse);
+}
+
+
+const getApiUrl = (address) => {
+    console.log(`${API_URL}?address=${address.replace(/[- ]+/g, "+")}&key=${API_KEY}`)
+    return `${API_URL}?address=${address.replace(/[- ]+/g, "+")}&key=${API_KEY}`;
+}
+const address = endereco;
+//console.log(Object.values(address).length)
+
+(async () => {
+    for (let i = 0; i < Object.values(endereco).length; i++) {
+    const apiUrl = getApiUrl(address[i]);
+    const data = await doRequest(apiUrl);
+
+
+
+    console.log(data.results[0].geometry.location);
+    }
+})();
